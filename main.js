33eeeb7d15e3a92ede613f20334c659390867554
TaylorHo <taylorho.ta@gmail.com>
2023-01-27 18:54:33 -0300

    feat: modal blue

---
+++ main.js
@@ -0,0 +1,14 @@
+
+function mostraModal() {
+
+  var modal = document.querySelector("#modal");
+  modal.classList.remove("escondido");
+
+}
+
+function escondeModal() {
+
+  var modal = document.querySelector("#modal");
+  modal.classList.add("escondido");
+
+}
