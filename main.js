895f4b5120e9258f6798808f1a734683abd12f59
TaylorHo <taylorho.ta@gmail.com>
2023-02-16 19:52:37 -0300

    feat: new class

---
+++ main.js
@@ -0,0 +1,20 @@
+
+function mostraModal() {
+
+    var modal = document.querySelector("#modal");
+    modal.classList.remove("escondido");
+  
+    var overlay = document.querySelector("#fundo-preto");
+    overlay.classList.remove("escondido");
+  
+  }
+  
+  function escondeModal() {
+  
+    var modal = document.querySelector("#modal");
+    modal.classList.add("escondido");
+  
+    var overlay = document.querySelector("#fundo-preto");
+    overlay.classList.add("escondido");
+  
+  }
